960bcc33f34dd7136fa4f490c7fa3e12b8ff6545
Martin Czygan <martin.czygan@gmail.com>
2019-12-15 13:53:38 +0100

    some meta

--- indigo.py
+++ indigo.py
@@ -14,7 +14,9 @@
 
 import argparse
 import collections
+import datetime
 import fileinput
+import hashlib
 import json
 import random
 
@@ -40,6 +42,15 @@ class Reservoir:
             if m < self.size:
                 self.storage[key][m] = value
 
+    def unique_storage(self):
+        """
+        Return storage array, reduced to unique values.
+        """
+        us = collections.defaultdict(set)
+        for k, v in self.storage.items():
+            us[k] = set(v)
+        return us
+
     def __str__(self):
         return '<Reservoir with {} keys, size={}>'.format(len(self.storage), self.size)
 
@@ -52,14 +63,19 @@ def count_keys(doc, counters=None, samples=None, prefix=''):
     if samples is None:
         samples = Reservoir()
 
-    for k, v in doc.items():
-        key = '{}{}'.format(prefix, k)
-        counters[key] += 1
-        if isinstance(v, dict):
-            prefix = '{}.'.format(key)
-            count_keys(v, counters=counters, samples=samples, prefix=prefix)
-        else:
-            samples.add(key, v)
+    if isinstance(doc, dict):
+        for k, v in doc.items():
+            key = '{}{}'.format(prefix, k)
+            counters[key] += 1
+            if isinstance(v, dict):
+                count_keys(v, counters=counters, samples=samples, prefix='{}.'.format(key))
+            elif isinstance(v, list):
+                for item in v:
+                    count_keys(item, counters=counters, samples=samples, prefix='{}[].'.format(key))
+            else:
+                samples.add(key, v)
+    else:
+        return
 
 class SetEncoder(json.JSONEncoder):
     """
@@ -86,16 +102,31 @@ def main():
     # A reservoir for examples.
     samples = Reservoir(size=1024)
 
+    # Total number of documents.
+    total = 0
+
+    # Checksum as we go.
+    sha1 = hashlib.sha1()
+
     # If you would call fileinput.input() without files it would try to process all arguments.
     # We pass '-' as only file when argparse got no files which will cause fileinput to read from stdin
     for line in fileinput.input(files=args.files if len(args.files) > 0 else ('-', )):
+        total += 1
+        sha1.update(line.encode('utf-8'))
         doc = json.loads(line)
         count_keys(doc, counters=counters, samples=samples)
 
     result = {
+        'meta': {
+            'size': samples.size,
+            'date': datetime.datetime.now().isoformat(),
+            'total': total,
+            'sha1': sha1.hexdigest(),
+        },
         'c': counters,
         's': samples.storage,
-        'u': samples.uniq,
+        'u': samples.unique_storage(),
+        'v': samples.uniq,
     }
     print(json.dumps(result, cls=SetEncoder))
 
